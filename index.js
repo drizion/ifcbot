e780f8b73a2d413daae5eacceb2ac91daf081d05
Futurity <vocalmusicstudio20@gmail.com>
2022-03-31 18:06:26 +0000

    optimize index

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-import { Boom } from '@hapi/boom'
 import P from 'pino'
 import makeWASocket, { delay, getContentType, DisconnectReason, fetchLatestBaileysVersion, makeInMemoryStore, useSingleFileAuthState } from '@adiwajshing/baileys'
-import * as fs from 'fs'
-import ffmpeg from 'fluent-ffmpeg'
-import { exec } from 'child_process'
-
-const store = makeInMemoryStore({ logger: P().child({ level: 'error', stream: 'store' }) })
-store.readFromFile('./baileys_store_multi.json')
-// save every 10s
-setInterval(() => {
-	store.writeToFile('./baileys_store_multi.json')
-}, 10_000)
-
+import setMediaType from './lib/setMediaType.js'
 
 const prefix = "#"
-const { state, saveState } = useSingleFileAuthState('./auth_info_multi.json')
 
-// start a connection
+const store = makeInMemoryStore({ logger: P().child({ level: 'error', stream: 'store' }) })
+store.readFromFile('./store/baileys_store_multi.json')
+    setInterval(() => {
+	    store.writeToFile('./store/baileys_store_multi.json')
+    }, 10_000)
+
+const { state, saveState } = useSingleFileAuthState('./auth/auth_info_multi.json')
+
 const startSock = async() => {
-	// fetch latest version of WA Web
 	const { version, isLatest } = await fetchLatestBaileysVersion()
 	console.log(`using WA v${version.join('.')}, isLatest: ${isLatest}`)
-
 	const sock = makeWASocket["default"]({
 		version,
 		logger: P({ level: 'error' }),
@@ -30,10 +23,9 @@ const startSock = async() => {
 	})
 	store.bind(sock.ev)
 	
-
-	sock.ev.on('messages.upsert', async m => {
-		
+	sock.ev.on('messages.upsert', async m => {	
 		try {
+            if (m.type !== 'notify') return
             const msg = m.messages[0] // last message
             const sMsg = JSON.stringify(msg,null,2) // stringified message
             const from = msg.key.remoteJid // chat id
@@ -41,89 +33,46 @@ const startSock = async() => {
             const ownerNumber = "555190052219@s.whatsapp.net"
 			const sender = msg.key.participant || msg.key.remoteJid
             const isOwner = sender.includes(ownerNumber) || false
-
-            var body = ''
-            if (m.type === 'notify') {
-                const type = getContentType(msg.message) // get message type and format body
-                const messageType = Object.keys(msg.message)[0]
-                    let isMediaType = ''
-                const isMedia = (type === 'imageMessage' || type === 'videoMessage')
-                    if(isMedia){ isMediaType = "0" }
-                const isQuotedImage = type === 'extendedTextMessage' && sMsg.includes('imageMessage')
-                    if(isQuotedImage){ isMediaType = "1" }
-                const isQuotedVideo = type === 'extendedTextMessage' && sMsg.includes('videoMessage')
-                    if(isQuotedVideo){ isMediaType = "2" }
-                const isQuotedSticker = type === 'extendedTextMessage' && sMsg.includes('stickerMessage')
-                    if(isQuotedSticker) { isMediaType = "3"}
-                const isQuotedAudio = type === 'extendedTextMessage' && sMsg.includes('audioMessage')
-                    if(isQuotedAudio){ isMediaType = "4" }
-                switch(type){
-                    case 'conversation':
-                        body += msg.message?.conversation
+            const MediaType = setMediaType(msg.message)
+            const body = MediaType.body
+            const isMedia = MediaType.isMedia
+            const isQuotedImage = MediaType.isQuotedImage
+            const isQuotedVideo = MediaType.isQuotedVideo
+            const isQuotedAudio = MediaType.isQuotedAudio
+            const isQuotedSticker = MediaType.isQuotedSticker
+            const isMediaType = MediaType.isMediaType
+            // if is a command
+            if(body.startsWith(prefix)){
+                const command =  body.slice(1).trim().split(' ')[0].toLowerCase()
+                switch(command){ // commands
+                    case '':
                     break
-                    case 'imageMessage':
-                        body += msg.message?.imageMessage?.caption
+                    case 'menu':
+                        await sock.sendMessage(from, { text: 'bem vindo ao menu!\n\nessa é a versão de testes da próxima atualização do wabot.net\n\naté o momento, o único comando funcionando é #fig' })
                     break
-                    case 'videoMessage':
-                        body += msg.message?.videoMessage?.caption
+                    case 'add':
+                        console.log("command add")
                     break
-                    case 'extendedTextMessage':
-                        body += msg.message?.extendedTextMessage?.text
+                    case 'msg':
+                        if(!isOwner) return
+                        await sock.sendMessage(from, { text: JSON.stringify(msg,null,2)})                
                     break
-                    case 'buttonsResponseMessage':
-                        body += msg.message?.buttonsResponseMessage?.selectedButtonId
-                    break
-                    case 'liveLocationMessage':
-                        body += msg.message?.liveLocationMessage?.caption
-                    break
-                    case 'listResponseMessage':
-                        body += msg.message?.listResponseMessage?.singleSelectReply?.selectedRowId
+                    case 'status':
+                        await sock.sendMessage(from, { text: JSON.stringify(MediaType,null,2)})
                     break
                     default:
+                        await sock.sendMessage(from, { text: `desculpe, o comando ${prefix}${command} não existe...`})
                     break
                 }
-                
-                // if is a command
-                if(body.startsWith(prefix)){
-                    const command =  body.slice(1).trim().split(' ')[0].toLowerCase()
-                    switch(command){ // commands
-                        case '':
-                        break
-                        case 'menu':
-                            await sock.sendMessage(from, { text: 'bem vindo ao menu!\n\nessa é a versão de testes da próxima atualização do wabot.net\n\naté o momento, o único comando funcionando é #fig' })
-                        break
-                        case 'add':
-                            console.log("command add")
-                        break
-                        case 'msg':
-                            if(!isOwner) return
-                            await sock.sendMessage(from, { text: JSON.stringify(msg,null,2)})                
-                        break
-                        case 'status':
-                            let obj = {
-                                "isMedia": isMedia,
-                                "isQuotedImage": isQuotedImage,
-                                "isQuotedVideo": isQuotedVideo,
-                                "isQuotedAudio": isQuotedAudio,
-                                "isQuotedSticker": isQuotedSticker,
-                                "isOwner": isOwner
-                            }
-                            await sock.sendMessage(from, { text: JSON.stringify(obj,null,2)})
-                        break
-                        default:
-                            await sock.sendMessage(from, { text: `desculpe, o comando ${prefix}${command} não existe...`})
-                        break
-                    }
-                    console.log(`[cmd]: (${command}) | ${body} | ${sender}`)
-                } else {
-                    if(body === '') return
+                console.log(`[cmd]: (${command}) | ${body} | ${sender}`)
+            } else {
+                if(body !== '') {
                     console.log(`[msg]: ${body} | ${sender.replace('@s.whatsapp.net','')}`)
                 }
             }
         } catch (e) {
             console.log('error: ' + e)
         }
-
 	})
 
 	sock.ev.on('connection.update', (update) => {
@@ -136,12 +85,9 @@ const startSock = async() => {
 				console.log('Connection closed. You are logged out.')
 			}
 		}
-        
 		console.log('connection update', update)
 	})
-	// listen for when the auth credentials is updated
-	sock.ev.on('creds.update', saveState)
-
+		sock.ev.on('creds.update', saveState)
 	return sock
 }
 
