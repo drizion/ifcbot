dc389363f5c4cb8148de479993a607d0e16087ff
Gabriel da Silva <vocalmusicstudio20@gmail.com>
2022-12-30 16:13:52 +0000

    final version

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import makeWASocket, { DisconnectReason, fetchLatestBaileysVersion, makeInMemoryStore, BufferJSON, useMultiFileAuthState  } from '@adiwajshing/baileys'
+import makeWASocket, { DisconnectReason, fetchLatestBaileysVersion, makeInMemoryStore, BufferJSON, useMultiFileAuthState } from '@adiwajshing/baileys'
 import * as fs from 'fs'
 import P from 'pino'
 import setMediaType from './lib/setMediaType.js'
@@ -14,122 +14,122 @@ import superior from './src/commands/superior.js'
 
 const dic = JSON.parse(fs.readFileSync('./lib/dictionary.json'))
 const prefix = dic.config.prefix
-const store = makeInMemoryStore({ logger: P().child({ level: 'silent', stream: 'store' }) })
+const store = makeInMemoryStore({})
 
 store.readFromFile('./session/baileys_store_multi.json')
 setInterval(() => {
-    store.writeToFile('./session/baileys_store_multi.json')
+  store.writeToFile('./session/baileys_store_multi.json')
 }, 10_000)
 
 const { state, saveCreds } = await useMultiFileAuthState('baileys_auth_info')
 
 const startSock = async () => {
-    const { version, isLatest } = await fetchLatestBaileysVersion()
-    console.log(`using WA v${version.join('.')}, isLatest: ${isLatest}`)
-    const sock = makeWASocket["default"]({
-        version,
-        logger: P({
-            level: 'fatal'
-        }),
-        printQRInTerminal: true,
-        auth: state
-    })
-    store.bind(sock.ev)
-    //
-    sock.ev.on('connection.update', (update) => {
-		const { connection, lastDisconnect } = update
-		if(connection === 'close') {
-			if((lastDisconnect?.error)?.output?.statusCode !== DisconnectReason.loggedOut) {
-				startSock()
-			} else {
-				console.log('Connection closed. You are logged out.')
-			}
-		}
-		console.log('connection update', update)
-	})
-    //
-    sock.ev.on('creds.update', saveCreds)
-    //
-    sock.ev.on('messages.upsert', async m => {
-        try {
-            if (m.type !== 'notify') return
-            const msg = m.messages[0]
-            const from = msg.key.remoteJid
-            const MediaType = setMediaType(msg.message)
-            const sender = msg.key.participant?.split(":")[0] || msg.key.remoteJid
-            const ownerNumber = "555190052219@s.whatsapp.net"
-            const isOwner = sender.includes(ownerNumber) || false
-            const isGroup = from.endsWith('@g.us')
-            const { body } = MediaType
-            const reply = async (message) => {
-                await sock.sendMessage(from, {
-                    text: message
-                }, {
-                    quoted: msg
-                })
-            }
-            
-            if (body.startsWith(prefix)) {
-                const command = body.slice(1).trim().split(' ')[0].toLowerCase()
-                switch (command) { // commands
-                    case '':
-                        break
-                    case 'oi':
-                    case 'start':
-                    case 'comecar':
-                    case 'começar':
-                        start(sock, from, prefix)    
-                    break
-                    case 'conhecer':
-                        conhecer(sock, from, prefix)
-                    break
-                    case 'integrado':
-                        integrado(sock, from, prefix)
-                    break
-                    case 'superior':
-                        superior(sock, from, prefix)
-                    break
-                    case 'local':
-                        await local(sock, from, prefix)  
-                        await voltarAoMenu(sock, from, prefix)  
-                    break
-                    case 'criador':
-                        criador(sock, from, prefix)    
-                    break
-                    case 'orientador':
-                        orientador(sock, from, prefix)
-                    break
-                    case 'horario':
-                        horario(sock, from, prefix)
-                    break
-                    case 'msg':
-                        if (!isOwner) return
-                        await sock.sendMessage(from, {
-                            text: JSON.stringify(msg, null, 2)
-                        })
-                        break
-                    case 'status':
-                        await sock.sendMessage(from, {
-                            text: JSON.stringify(MediaType, null, 2)
-                        })
-                        break
-                    default:
-                        await sock.sendMessage(from, {
-                            text: `desculpe, o comando ${prefix}${command} não existe...`
-                        })
-                        break
-                }
-                console.log(`[cmd]: (${command}) | ${body} | ${sender}`)
-            } else {
-                if (body !== '') {
-                    console.log(`[msg]: ${body} | ${sender.replace('@s.whatsapp.net','')}`)
-                }
-            }
-        } catch (e) {
-            console.log('error: ' + e)
+  const { version, isLatest } = await fetchLatestBaileysVersion()
+  console.log(`using WA v${version.join('.')}, isLatest: ${isLatest}`)
+  const sock = makeWASocket["default"]({
+    version,
+    logger: P({
+      level: 'fatal'
+    }),
+    printQRInTerminal: true,
+    auth: state
+  })
+  store.bind(sock.ev)
+  //
+  sock.ev.on('connection.update', (update) => {
+    const { connection, lastDisconnect } = update
+    if (connection === 'close') {
+      if ((lastDisconnect?.error)?.output?.statusCode !== DisconnectReason.loggedOut) {
+        startSock()
+      } else {
+        console.log('Connection closed. You are logged out.')
+      }
+    }
+    console.log('connection update', update)
+  })
+  //
+  sock.ev.on('creds.update', saveCreds)
+  //
+  sock.ev.on('messages.upsert', async m => {
+    try {
+      if (m.type !== 'notify') return
+      const msg = m.messages[0]
+      const from = msg.key.remoteJid
+      const MediaType = setMediaType(msg.message)
+      const sender = msg.key.participant?.split(":")[0] || msg.key.remoteJid
+      const ownerNumber = "555190052219@s.whatsapp.net"
+      const isOwner = sender.includes(ownerNumber) || false
+      const isGroup = from.endsWith('@g.us')
+      const { body } = MediaType
+      const reply = async (message) => {
+        await sock.sendMessage(from, {
+          text: message
+        }, {
+          quoted: msg
+        })
+      }
+
+      if (body.startsWith(prefix)) {
+        const command = body.slice(1).trim().split(' ')[0].toLowerCase()
+        switch (command) { // commands
+          case '':
+            break
+          case 'oi':
+          case 'start':
+          case 'comecar':
+          case 'começar':
+            start(sock, from, prefix)
+            break
+          case 'conhecer':
+            conhecer(sock, from, prefix)
+            break
+          case 'integrado':
+            integrado(sock, from, prefix)
+            break
+          case 'superior':
+            superior(sock, from, prefix)
+            break
+          case 'local':
+            await local(sock, from, prefix)
+            await voltarAoMenu(sock, from, prefix)
+            break
+          case 'criador':
+            criador(sock, from, prefix)
+            break
+          case 'orientador':
+            orientador(sock, from, prefix)
+            break
+          case 'horario':
+            horario(sock, from, prefix)
+            break
+          case 'msg':
+            if (!isOwner) return
+            await sock.sendMessage(from, {
+              text: JSON.stringify(msg, null, 2)
+            })
+            break
+          case 'status':
+            await sock.sendMessage(from, {
+              text: JSON.stringify(MediaType, null, 2)
+            })
+            break
+          default:
+            await sock.sendMessage(from, {
+              text: `desculpe, o comando ${prefix}${command} não existe...`
+            })
+            break
         }
-    })
-    return sock
+        console.log(`[cmd]: (${command}) | ${body} | ${sender}`)
+      } else {
+        if (body !== '') {
+          console.log(`[msg]: ${body} | ${sender.replace('@s.whatsapp.net', '')}`)
+        }
+      }
+    } catch (e) {
+      console.log('error: ' + e)
+    }
+  })
+  return sock
 }
 
 startSock()
--- src/commands/horario.js
+++ src/commands/horario.js
@@ -1,23 +1,22 @@
 import { horarioEM } from "../../lib/ifc.js"
 
 export default async(sock, from, prefix) => {
-    horarioEM().then(async (horario) => {
+    // horarioEM().then(async (horario) => {
         const buttons = [
             {buttonId: `${prefix}conhecer`, buttonText: {displayText: 'Voltar'}, type: 1}
         ]
         
         const buttonMessage = {
-            text: `O horário atualizado do ensino médio do IFC-CAS está disponível no link abaixo:\n${horario}`,
+            text: `O horário atualizado do ensino médio do IFC-CAS está disponível no link abaixo:\nhttps://docs.google.com/spreadsheets/d/e/2PACX-1vSTiNBKTBcj-1uF_KW9K2mqXD-RfzsRi2T-G_hT5KgB-5iA_3q9Ymk0jzuGq8zv5pI7B86HRdciZ_lG/pubhtml`,
             footer: '[Assistente do IFC-CAS]',
             buttons: buttons,
             headerType: 1
         }
-        
         await sock.sendMessage(from, buttonMessage)
-    }).catch(err => {
-        sock.sendMessage(from, {
-            text: "Algo deu errado ao buscar o horário do ensino médio... tente novamente mais tarde."
-        })
-        console.log(err);
-    })
+    // }).catch(err => {
+    //     sock.sendMessage(from, {
+    //         text: "Algo deu errado ao buscar o horário do ensino médio... tente novamente mais tarde."
+    //     })
+    //     console.log(err);
+    // })
 }
